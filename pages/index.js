82b48545a468576f8651d3932f5077e0de51da78
Rodigee <reiddelfeld@gmail.com>
2024-09-29 13:09:55 -0500

    Changed game to render components at full screen width.

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import TimelineGame from '../components/TimelineGame'
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Head>
         <title>Historical Timeline Game</title>
         <meta name="description" content="Test your knowledge of historical events with our timeline game!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto px-4">
+      <main className="flex-grow w-full">
         <TimelineGame />
       </main>
     </div>
