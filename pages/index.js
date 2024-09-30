e5214f4948b92f1e428fe512c8ba12549e6891fe
Rodigee <reiddelfeld@gmail.com>
2024-09-30 09:57:36 -0500

    Changed favicon from Vercel default to a Roman T.

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,10 @@ export default function Home() {
       <Head>
         <title>Historical Timeline Game</title>
         <meta name="description" content="Test your knowledge of historical events with our timeline game!" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="flex-grow w-full">
